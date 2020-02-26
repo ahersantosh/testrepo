53ad81d5e6978c15c04ab3061043a4d43c56f10f
ahersantosh <ahersantosh@yahoo.co.in>
2020-02-26 06:05:21 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+function toUpperCase(input) {
+   // Returns true if the input is either undefined, null, or empty, false otherwise
+   if(input){
+   console.log(str.toUpperCase());
+   }
+}
+// Export to make the function available to other packages
+module.exports = toUpperCase;
